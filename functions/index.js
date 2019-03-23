9ca97fd43f0e96d62b9f85a9ab226bddbb8a19c1
Sai Kiran Dasika <kirandasika30@gmail.com>
2019-03-23 17:23:47 -0500

    added test files and updated addCoordinate function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
+var coordinatesCollection = db.collection('coordinates');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.addCoordinate = functions.https.onRequest((request, response) => {
+    const userId = request.body.userId;
+    const timestamp = request.body.timestamp;
+    const latitude = request.body.latitude;
+    const longitude = request.body.longitude;
+
+
+    coordinatesCollection.doc(userId).collection('latest').add({
+        coord: new admin.firestore.GeoPoint(latitude, longitude),
+        timestamp: Date.parse(timestamp),
+    })
+        .then(reference => {
+            return response.status(201).send({ reference: reference });
+        })
+        .catch(reason => {
+            return response.status(500).send({
+                error: 'could not add coordinate to the database'
+            })
+        })
 });
